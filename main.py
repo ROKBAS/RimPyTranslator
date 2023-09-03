b69c0ec0e41ec4b44e9625c2296cff5e8d60155b
ROKBAS <anton.fomi@ya.ru>
2023-09-03 23:23:19 +0300

    main: up app version

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils import SETTINGS_PATH, initiate_settings
 if os.path.exists("RimPyTranslate.log"):
     os.remove("RimPyTranslate.log")
 
-version = "0.2.5"
+version = "0.2.6"
 log_level = logging.DEBUG
 logging.basicConfig(
     format="%(levelname)s: %(message)s", level=log_level, filename="RimPyTranslate.log"
