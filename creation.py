0b469e35d880f1b6a970392e331ccadac4536b3b
ROKBAS <anton.fomi@ya.ru>
2023-09-03 21:27:40 +0300

    creation: added patch simple method

--- creation.py
+++ creation.py
@@ -16,6 +16,14 @@ def create_keyed_xml(keyed_strings, path):
         _string.text = string
     ET.ElementTree(root).write(path, encoding="utf-8", xml_declaration=True, short_empty_elements=False)
 
+
+def create_patch_xml(patch_strings, path):
+    root = ET.Element("LanguageData")
+    for _id, string in patch_strings:
+        _string = ET.SubElement(root, _id)
+        _string.text = string
+    ET.ElementTree(root).write(path, encoding="utf-8", xml_declaration=True, short_empty_elements=False)
+
 def create_strings_text(string_strings, path):
     with open(path, "w+") as file:
         file.writelines(string_strings)
