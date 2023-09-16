a2b75ce503c658de6ff89d8091a6782304b321cc
ROKBAS <anton.fomi@ya.ru>
2023-09-16 17:50:13 +0300

    base: prepare languages from selected langs

--- analyzers/base.py
+++ analyzers/base.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 
+from languages import Languages
+
 logger = logging.getLogger(__name__)
 
 
@@ -9,10 +11,15 @@ class Analyzer:
         languages_path = path_object.joinpath("Languages")
         if not languages_path.exists():
             languages_path.mkdir(mode=777, exist_ok=True)
-        prefered_language = languages_path.joinpath("Russian")
+
+        prefered_language = languages_path.joinpath(
+            Languages[self.original_language].value
+        )
         if not prefered_language.exists():
             prefered_language.mkdir(mode=777, exist_ok=True)
-        creator_language = languages_path.joinpath("English")
+        creator_language = languages_path.joinpath(
+            Languages[self.translation_language].value
+        )
         if not creator_language.exists():
             creator_language.mkdir(mode=777, exist_ok=True)
         return prefered_language, creator_language
